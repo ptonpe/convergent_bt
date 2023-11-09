db2cc348333f6973c39b8d08b4d273fb1b106ed3
Shreya Varma <shreyavarma99@gmail.com>
2023-11-09 13:05:18 -0600

    commit

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,67 +1,135 @@
-import { StyleSheet, Text, View, SafeAreaView, Pressable } from "react-native"
-import React from 'react'
-import { LinearGradient } from "expo-linear-gradient"
-import { Entypo } from '@expo/vector-icons';
-import { FontAwesome } from '@expo/vector-icons';
+import { StyleSheet, Text, View, SafeAreaView, Pressable } from "react-native";
+import React, { useEffect } from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import { Entypo } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import * as AppAuth from "expo-app-auth";
+import { AsyncStorage } from "@react-native-async-storage/async-storage";
+import { useNavigation } from "@react-navigation/native";
 
 const LoginScreen = () => {
-    const authenticate = () => {
-        
+  const navigation = useNavigation();
+  useEffect(() => {
+    const checkTokenValidity = async () => {
+      const accessToken = await AsyncStorage.getItem("token");
+      const expirationDate = await AsyncStorage.getItem("expirationDate");
+      console.log("accessToken", accessToken);
+      console.log("expirationDate", expirationDate);
+
+      if (accessToken && expirationDate) {
+        const currentTime = Date.now();
+        if (currentTime < parseInt(expirationDate)) {
+          //token is still valid
+          navigation.replace("Main");
+        } else {
+          //token is expired so remove from async storage
+          AsyncStorage.removeItem("token");
+          AsyncStorage.removeItem("expirationDate");
+        }
+      }
+    };
+
+    checkTokenValidity();
+  }, []);
+  async function authenticate() {
+    const config = {
+      issuer: "https://accounts.spotify.com",
+      clientId: "4044d1f8ee084244b18600ef0212740e",
+      scopes: [
+        "user-read-email",
+        "user-library-read",
+        "user-read-recently-played",
+        "user-top-read",
+        "playlist-read-private",
+        "playlist-read-collaborative",
+        "playlist-modify-public", //or playlist-modify-private
+      ],
+      redirectUrl: "exp://localhost:8081/--/spotify-auth-callback",
+    };
+    const result = await AppAuth.authAsync(config);
+    console.log(result);
+    if (result.accessToken) {
+      const expirationDate = new Date(
+        result.accessTokenExpirationDate
+      ).getTime();
+      AsyncStorage.setItem("token", result.accessToken);
+      AsyncStorage.setItem("expirationDate", expirationDate.toString());
+      navigation.navigate("Main");
     }
-    return (
-        <LinearGradient colors={["#040306", "#131624"]} style={{ flex: 1 }}>
-            <SafeAreaView>
-                <View style={{ height: 80 }} />
-                <Entypo style={{ textAlign: "center" }} name="spotify" size={80} color="white" />
-                <Text style={{
-                    color: "white",
-                    fontSize: 40,
-                    fontWeight: "bold",
-                    textAlign: "center",
-                    marginTop: 40
-                }}
-                >
-                    Spotify Recommender
-                </Text>
-                <View style={{ height: 80 }} />
-                <Pressable
-                onPress={authenticate}
-                    style={{
-                        backgroundColor: "#1DB954",
-                        padding: 10, marginLeft: "auto",
-                        marginRight: "auto",
-                        width: 300,
-                        borderRadius: 25,
-                        alignItems: "center",
-                        justifyContent: "center",
-                        marginVertical:10
-                    }}
-                >
-                    <Text>Sign in!</Text>
-                </Pressable>
+  }
+  return (
+    <LinearGradient colors={["#040306", "#131624"]} style={{ flex: 1 }}>
+      <SafeAreaView>
+        <View style={{ height: 80 }} />
+        <Entypo
+          style={{ textAlign: "center" }}
+          name="spotify"
+          size={80}
+          color="white"
+        />
+        <Text
+          style={{
+            color: "white",
+            fontSize: 40,
+            fontWeight: "bold",
+            textAlign: "center",
+            marginTop: 40,
+          }}
+        >
+          Spotify Recommender
+        </Text>
+        <View style={{ height: 80 }} />
+        <Pressable
+          onPress={authenticate}
+          style={{
+            backgroundColor: "#1DB954",
+            padding: 10,
+            marginLeft: "auto",
+            marginRight: "auto",
+            width: 300,
+            borderRadius: 25,
+            alignItems: "center",
+            justifyContent: "center",
+            marginVertical: 10,
+          }}
+        >
+          <Text>Sign in!</Text>
+        </Pressable>
 
-                <Pressable style={{
-                    backgroundColor: "#131624", padding: 10, marginLeft: "auto",
-                    marginRight: "auto",
-                    width: 300,
-                    borderRadius: 25,
-                    alignItems: "center",
-                    justifyContent: "center",
-                    flexDirection:"row",
-                    marginVertical:10,
-                    borderColor:"#C0C0C0",
-                    borderWidth:0.8
-                }}>
-                    <FontAwesome name="mobile-phone" size={24} color="white" />
-                    <Text style={{fontWeight:"500",color:"white", textAlign:"center", flex:1}}> Use phone number</Text>
-
-                </Pressable>
-            </SafeAreaView>
-        </LinearGradient>
-
-    );
+        <Pressable
+          style={{
+            backgroundColor: "#131624",
+            padding: 10,
+            marginLeft: "auto",
+            marginRight: "auto",
+            width: 300,
+            borderRadius: 25,
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "row",
+            marginVertical: 10,
+            borderColor: "#C0C0C0",
+            borderWidth: 0.8,
+          }}
+        >
+          <FontAwesome name="mobile-phone" size={24} color="white" />
+          <Text
+            style={{
+              fontWeight: "500",
+              color: "white",
+              textAlign: "center",
+              flex: 1,
+            }}
+          >
+            {" "}
+            Use phone number
+          </Text>
+        </Pressable>
+      </SafeAreaView>
+    </LinearGradient>
+  );
 };
 
-export default LoginScreen
+export default LoginScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
